74b75326e79f1378bf0cac7bf95ed5cc52e70607
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2023-01-23 16:27:56 -0300

    Footer component created and styled

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { Header } from "./src/components/Header";
 import { StyleSheet, View } from "react-native";
+import { Footer } from "./src/components/Footer";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Header />
+      <Footer />
     </View>
   );
 }
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,56 @@
+import { Image, StyleSheet, Text, View } from "react-native";
+
+const Footer = () => {
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.logo}
+        source={require("../../../assets/Motors_shop_footer.png")}
+      />
+      <Text style={styles.text}>© 2022 - Todos os direitos reservados.</Text>
+      <Text style={styles.link}>^</Text>
+    </View>
+  );
+};
+
+export { Footer };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#0B0D0D",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingTop: 45,
+    paddingBottom: 45,
+    paddingLeft: 20,
+    paddingRight: 20,
+    height: 310.34,
+  },
+  logo: {
+    width: 153.02,
+    height: 26.34,
+  },
+  text: {
+    color: "#FFFFFF",
+    fontWeight: "400",
+    fontSize: 14,
+    lineHeight: 24,
+  },
+  link: {
+    width: 53,
+    height: 50,
+    backgroundColor: "#212529",
+    borderRadius: 4,
+    color: "#FFFFFF",
+    fontWeight: "900",
+    fontSize: 16,
+    lineHeight: 18,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "center",
+    padding: 20
+  },
+});
