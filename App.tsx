b2071ebe6a34b06c44e308adb5887fdab190a344
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2023-01-23 17:53:51 -0300

    FormSignIn component created and styled

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
 import { Header } from "./src/components/Header";
 import { StyleSheet, View } from "react-native";
 import { Footer } from "./src/components/Footer";
+import { FormSignIn } from "./src/components/FormSignIn";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Header />
+      
+      <View style={styles.main}>
+        <FormSignIn />
+      </View>
+
       <Footer />
     </View>
   );
@@ -16,4 +22,11 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
   },
+  main: {
+    backgroundColor: "#E9ECEF",
+    paddingTop: 48,
+    paddingBottom: 48,
+    paddingLeft: 10,
+    paddingRight: 10,
+  }
 });
---
+++ src/components/FormSignIn/index.tsx
@@ -0,0 +1,135 @@
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+
+const FormSignIn = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Login</Text>
+
+      <Text style={styles.label}>Usuário</Text>
+      <TextInput
+        placeholder="Digitar usuário"
+        keyboardType="default"
+        style={styles.input}
+        placeholderTextColor="#868E96"
+      />
+      <Text style={styles.label}>Senha</Text>
+      <TextInput
+        placeholder="Digitar senha"
+        keyboardType="visible-password"
+        style={styles.input}
+        placeholderTextColor="#868E96"
+      />
+
+      <Text style={styles.linkMyPassword}>Esqueci minha senha</Text>
+
+      <View style={styles.viewButtons}>
+        <View style={styles.buttonBlue}>
+          <Text style={styles.buttonBlueText}>Entrar</Text>
+        </View>
+
+        <Text style={styles.textAccount}>Ainda não possui conta?</Text>
+
+        <View style={styles.buttonWhite}>
+          <Text style={styles.buttonWhiteText}>Cadastrar</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export { FormSignIn };
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 44,
+    paddingBottom: 44,
+    paddingLeft: 28,
+    paddingRight: 28,
+    width: 343,
+    height: 542,
+    backgroundColor: "#FDFDFD",
+    borderRadius: 4,
+  },
+  title: {
+    color: "#000000",
+    fontWeight: "500",
+    fontSize: 24,
+    lineHeight: 30,
+  },
+  label: {
+    fontWeight: "500",
+    fontSize: 14,
+    lineHeight: 17,
+    color: "#212529",
+    paddingTop: 22.5,
+    paddingBottom: 7,
+  },
+  input: {
+    borderWidth: 2,
+    borderColor: "#E9ECEF",
+    paddingLeft: 16,
+    paddingRight: 16,
+    borderRadius: 4,
+    height: 48,
+    fontSize: 16,
+  },
+  linkMyPassword: {
+    color: "#495057",
+    fontWeight: "500",
+    fontSize: 14,
+    lineHeight: 24,
+    textAlign: "right",
+    paddingRight: 37,
+    paddingTop: 10,
+    paddingBottom: 20,
+  },
+  buttonBlue: {
+    backgroundColor: "#4529E6",
+    paddingTop: 12,
+    paddingBottom: 12,
+    paddingLeft: 28,
+    paddingRight: 28,
+    width: 286.09,
+    height: 48,
+    borderWidth: 1.5,
+    borderColor: "#4529E6",
+    borderRadius: 4,
+  },
+  buttonBlueText: {
+    fontWeight: "600",
+    fontSize: 16,
+    color: "#FFFFFF",
+    textAlign: "center",
+  },
+  textAccount: {
+    fontWeight: "400",
+    fontSize: 14,
+    lineHeight: 24,
+    color: "#495057",
+    textAlign: "center",
+  },
+  buttonWhite: {
+    paddingTop: 12,
+    paddingBottom: 12,
+    paddingLeft: 28,
+    paddingRight: 28,
+    width: 286.09,
+    height: 48,
+    borderWidth: 2,
+    borderColor: "#ADB5BD",
+    borderRadius: 4,
+  },
+  buttonWhiteText: {
+    fontWeight: "600",
+    fontSize: 16,
+    color: "#0B0D0D",
+    textAlign: "center",
+  },
+  viewButtons: {
+    height: 168,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+});
